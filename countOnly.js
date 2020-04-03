327aa99d8e978b5012a3338e4f7b9f074ce91872
Eduardo <Eduardo0321@gmail.com>
2020-04-03 06:59:03 +0000

    count the number of time a specific value in object

--- countOnly.js
+++ countOnly.js
@@ -34,6 +34,7 @@ let assertEqual = function(actual, expected) {
   
   const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
   
+  
   assertEqual(result1["Jason"], 1);
   assertEqual(result1["Karima"], undefined);
   assertEqual(result1["Fang"], 2);
