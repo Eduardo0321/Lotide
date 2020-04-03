1692ca813a0476f0746e3886cff37a9aa543aba8
Eduardo <Eduardo0321@gmail.com>
2020-04-03 05:27:20 +0000

    Fct compares arrays with assertion message to confirm

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,12 @@
-let assertArraysEquals = function(array1, array2) {
-  if (JSON.stringify(array1) === JSON.stringify(array2)) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-} else if (JSON.stringify(array1) !== JSON.stringify(array2)) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+let assertArraysEqual = function(actual, expected) {
+  if (JSON.stringify(actual) === JSON.stringify(expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+} else if (JSON.stringify(actual) !== JSON.stringify(expected)) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 }
 
-assertArraysEquals([1, 2, 3], [1, 2, 3]) // => true
-assertArraysEquals([1, 2, 3], [3, 2, 1]) // => false
-assertArraysEquals(["1", "2", "3"], ["1", "2", "3"]) // => true
-assertArraysEquals(["1", "2", "3"], ["1", "2", 3]) // => false
+assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
