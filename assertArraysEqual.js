78a05ba88958c33f2fe90d34e7b63af9357ea5c1
Eduardo0321 <58193445+Eduardo0321@users.noreply.github.com>
2020-04-28 17:57:07 -0400

    Update assertArraysEqual.js
    Refactored and eslinted code. Added the eqArrays fct too.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,16 @@
+
+const eqArrays = require("./eqArrays");
+
 let assertArraysEqual = function(actual, expected) {
   if (JSON.stringify(actual) === JSON.stringify(expected)) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-} else if (JSON.stringify(actual) !== JSON.stringify(expected)) {
+  } else if (JSON.stringify(actual) !== JSON.stringify(expected)) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
-}
+};
+module.exports = assertArraysEqual;
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
+assertArraysEqual(eqArrays[1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual(eqArrays[1, 2, 3], [3, 2, 1]); // => false
+assertArraysEqual(eqArrays["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(eqArrays["1", "2", "3"], ["1", "2", 3]); // => false
