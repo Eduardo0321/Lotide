0f6caf647ae58a2e6b73ab603f96e9fc10af1868
Eduardo0321 <58193445+Eduardo0321@users.noreply.github.com>
2020-04-29 15:45:51 -0400

    Update letterPositions.js
    Lint version refactored and tried new tests.

--- letterPositions.js
+++ letterPositions.js
@@ -1,32 +1,24 @@
 let assertArraysEqual = function(actual, expected) {
   if (JSON.stringify(actual) === JSON.stringify(expected)) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-} else if (JSON.stringify(actual) !== JSON.stringify(expected)) {
+  } else if (JSON.stringify(actual) !== JSON.stringify(expected)) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
-}
+};
 
-let eqArrays = function(array1, array2) {
-  if (JSON.stringify(array1) === JSON.stringify(array2)) {
-    return true;
-}
-}
 
 const letterPositions = function(sentence) {
-  const results = {};
-  for(letters of sentence) {
-      results[letters] = [];
+  let results = {};
+  for (let letter of sentence) {
+    results[letter] = [];
   }
-  for(var b in results) {
-      for(var a = 0; a < sentence.length; a++) {
-          if(b === sentence[a]) {
-              results[b].push(a);
-          }
+  for (let value in results) {
+    for (let i = 0; i < sentence.length; i++) {
+      if (value === sentence[i]) {
+        results[value].push(i);
       }
+    }
   }
-  
-
-
   return results;
 };
 
