dee28c6aaa66bf22f9615c8ce8075ad6ffb34d25
Eduardo0321 <58193445+Eduardo0321@users.noreply.github.com>
2020-05-01 22:57:45 -0400

    Update findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -6,14 +6,12 @@ let assertEqual = function(actual, expected) {
       }
   }
 
-const findKeyByValue = function(obj, value) {
-   for (let key of value) {
-     if (obj[key] === value) {
-       return key
-     }
-   }
-   return undefined;
- };
+  const findKeyByValue = function(object, value) {
+    let genres = Object.keys(object);
+    for (let genre of genres) {
+      if (object[genre] === value) return genre;
+    }
+  };
 
 const bestTVShowsByGenre = { 
    sci_fi: "The Expanse",
@@ -23,8 +21,8 @@ const bestTVShowsByGenre = {
  
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
 
 console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 console.log(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
